74d7b89355d1cdf025ed389506afb0b1c16ced59
UroKouShy <38764484+UroKouShy@users.noreply.github.com>
2018-05-26 07:47:54 +0200

    Update start.js

--- start.js
+++ start.js
@@ -8,7 +8,7 @@ bot.on('ready', function () {
     console.log("Je suis connecté !")
   })
   
-  bot.login('process.env.TOKEN')
+  bot.login(process.env.TOKEN)
 
   bot.on('message', message => {
     if (message.content === prefix + 'support'){
